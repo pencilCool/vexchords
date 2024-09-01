03b89d5ecb3e6f15156df92ce814f5c223ee0d6a
pencilCool <yhtangcoder@gmail.com>
2024-09-01 21:53:02 +0800

    update

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
  * Mohit Muthanna Cheppudira -- http://0xfe.blogspot.com
  */
 
-import ChordBox from './chordbox';
-import { POSITIONS, SHAPES, build } from './builder';
+import ChordBox from "./chordbox.js";
+import { POSITIONS, SHAPES, build } from "./builder.js";
 
 function draw(sel, chord, opts) {
   return new ChordBox(sel, opts).draw(chord);
 }
 
-export {
-  POSITIONS, SHAPES, ChordBox, build, draw,
-};
+export { POSITIONS, SHAPES, ChordBox, build, draw };
